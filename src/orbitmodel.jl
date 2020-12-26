a237f0a5cec920a8a1be74d629e97c92b14a3da2
Klaus Crusius <klaus.crusius@web.de>
2020-12-26 11:49:13 +0100

    save state owith NBodySimulator

--- src/orbitmodel.jl
+++ src/orbitmodel.jl
@@ -57,6 +57,14 @@ end
 
 struct SolarSystemParameters <: PotentialParameters
     spk
+    t0
+    u0
+    v0
+end
+
+function SolarSystemParameters(spk, t0)
+    st = state(spk, t0, earth)
+    SolarSystemParameters(spk, t0, st[1], st[2])
 end
 
 import NBodySimulator.get_accelerating_function
@@ -67,7 +75,12 @@ function get_accelerating_function(p::SolarSystemParameters, simulation::NBodySi
     gps = grav_param(sun)
     gpj = grav_param(jupiter)
     spk = p.spk
-    t0 = TDBEpoch("2020-12-23T21:00:00")
+    t0 = p.t0
+    u0 = p.u0
+    v0 = p.v0
+    r0 = mean_radius(earth) + 300
+    tt = 600
+
     function ssaccel(dv, u, v, t, i)
         #println("ssaccel(::$(typeof(dv)), ::$((u))")
 
@@ -75,32 +88,59 @@ function get_accelerating_function(p::SolarSystemParameters, simulation::NBodySi
             dv .= 0
         else
             tj = t0 + t*seconds
+            u = u[:,i]
+            v = v[:,i]
             pm = SVector{3}(position(spk, tj, earth, moon))
             ps = SVector{3}(position(spk, tj, earth, sun))
-            u = u[:,i]
-            um = u .- pm
-            us = u .- ps
-            gg = -gpe / norm(u)^3
+            ue = u
+            um = ue .- pm
+            us = ue .- ps
+            r = norm(ue)
+            s = norm(v)
+            gg = -gpe / r^3
             gm = -gpm / norm(um)^3
             gs = -gps / norm(us)^3
-            dve = accel2(spk, tj, 600)
             dp = gm * um + gs * us
-            println("ssaccel gearth $(gg * u)")
-            println("ssaccel gms    $dp")
-            println("ssaccel earth  $dve")
-            println("ssaccel delta  $(dp - dve)")
-            dv .= gg * u + dp - dve
-            dm = deviation(spk, tj, moon, gpm, u)
-            ds = deviation(spk, tj, sun, gps, u)
-            dj = deviation(spk, tj, jupiter_barycenter, gpj, u)
-            println("ssaccel dmoon  $(dm)")
-            println("ssaccel dsun   $(ds)")
-            println("ssaccel djupi  $(dj)")
-            println("ssaccel dall   $(dm + ds + dj)")
-            dv .= dj + ds + dm + gg * u
+            println("ssaccel gearth     $(gg * u)")
+            println("ssaccel g moon/sun $dp")
+            if r < 6000
+                dp .-= v / tt
+            end
+            dv .= gg * ue + dp
         end
         nothing
     end
+end
+
+function test2(;hrs=1hours, speed=1, angle=0)
+    t0 = TDBEpoch("2020-12-23T21:00:00")
+    p = SolarSystemParameters(spk, t0)
+    u0, v0 = p.u0, p.v0
+
+    gpe = grav_param(earth)
+    h0 = mean_radius(earth) + 400
+    ve = sqrt(gpe / h0)
+
+    z = zero(SVector{3})
+    s, c = sincosd(angle)
+    v = ve * speed
+    r = SVector(s * h0, c * h0, 0.0)
+    v = SVector(c*v, - s*v, 0.0)
+    mass = 1.25
+    body1 = MassBody(r, v, mass)
+    bearth = MassBody(z, z, 0.0)
+
+    pdict = Dict(:custom_potential_params => p)
+
+    system = PotentialNBodySystem([bearth, body1], pdict)
+    tspan = (0.0, toseconds(hrs))
+    simulation = NBodySimulation(system, tspan)
+
+    sim_result = run_simulation(simulation, rtol=1e-15, alg_hints=[:stiff])
+
+    #animate(sim_result, "eart2.gif", fps=2)
+
+    sim_result
 end
 
 function deviation(spk, tj, body, gpb, u)
@@ -126,34 +166,3 @@ function accel2(spk, t0, dt)
 end
 
 toseconds(hrs) = seconds(hrs).Δt
-
-function test2(;hrs=1hours, speed=1, angle=0)
-    gpe = grav_param(earth)
-    h0 = mean_radius(earth) + 400
-    v0 = sqrt(gpe / h0)
-
-    z = zero(SVector{3})
-    s, c = sincosd(angle)
-    v = v0 * speed
-    r = SVector(s * h0, c * h0, 0.0)
-    v = SVector(c*v, -s*v, 0.0)
-    mass = 1.25
-    body1 = MassBody(r, v, mass)
-    bearth = MassBody(z, z, 0.0)
-
-    parameters = SolarSystemParameters(spk)
-    pdict = Dict(:custom_potential_params => parameters)
-
-    system = PotentialNBodySystem([bearth, body1], pdict)
-    tspan = (0.0, toseconds(hrs))
-    simulation = NBodySimulation(system, tspan)
-
-    sim_result = run_simulation(simulation)
-
-    #animate(sim_result, "eart2.gif", fps=2)
-
-    sim_result
-
-
-
-end
