d85c307ebaa3961ea307236cbacafe97064a65a7
Klaus Crusius <klaus.crusius@web.de>
2021-01-08 21:18:10 +0100

    fixed startvalues

--- src/orbitmodel.jl
+++ src/orbitmodel.jl
@@ -203,16 +203,21 @@ H(p, q) = sum(v(p, t) * p) - L(q, v(p, t), t) = p^2 / m - p * velocity(origin, t
 =#
 
 function H(p, q, param)
-    t = q[4]
-    E = p[4]
-    Q = q[1:3]
-    P = p[1:3]
+    t = q[4]    # artifical coordinate to formulate H independent of time argument
+    E = p[4]    # corresponding energy term which keeps H constant
+    Q = q[1:3]  # coordinates relative to param.origin
+    P = p[1:3]  # linear momentum relative to param.inertial
     tj = param.t0 + t * seconds
+    #distances between satellite and center of bodies
     pos= [Q .- position(spk, tj, param.origin, param.bodies[i]) for i = 1:length(param.bodies)]
-    norm(P)^2 / 2param.m - dot(P, velocity(spk, tj, param.inertial, param.origin)) - param.m * sum(param.gravis ./ norm.(pos)) + E
+    # kinetec energy
+    K = norm(P)^2 / 2param.m - dot(P, velocity(spk, tj, param.inertial, param.origin))
+    # gravitational potential
+    V = - param.m * sum(param.gravis ./ norm.(pos))
+    K + V + E
 end
 
-
+# ∂H/∂p (velocity relative to param.origin)
 function dq(qdot, p, q, param::SolarSystemP, t)
     t = q[4]
     P = p[1:3]
@@ -220,6 +225,7 @@ function dq(qdot, p, q, param::SolarSystemP, t)
     qdot .= ([P / param.m - velocity(spk, tj, param.inertial, param.origin); 1.0])
 end
 
+# -∂H/∂q (force relative to param.initial)
 function dp(pdot, p, q, param::SolarSystemP, t)
     t = q[4]
     E = p[4]
@@ -227,11 +233,11 @@ function dp(pdot, p, q, param::SolarSystemP, t)
     P = p[1:3]
     tj = param.t0 + t * seconds
     n = length(param.bodies)
-    
+
     pos = [Q .- position(spk, tj, param.origin, param.bodies[i]) for i = 1:n]
     dp13 = -param.m * sum( param.gravis[i] / norm(pos[i])^3 * pos[i] for i in 1:n)
     dp4 = dot(P, accel(spk, tj, param.inertial, param.origin))
-    dp4 += param.m * sum(param.gravis[i] / norm(pos[i])^3 * dot(pos[i], accel(spk, tj, param.origin, param.bodies[i])) for i = 1:n) 
+    dp4 += param.m * sum(param.gravis[i] / norm(pos[i])^3 * dot(pos[i], accel(spk, tj, param.origin, param.bodies[i])) for i = 1:n)
     pdot .= ([dp13; dp4])
 end
 
@@ -245,9 +251,10 @@ function startvalues(ssp::SolarSystemP, t::Period, tv::Period, h, vfactor)
     spk = ssp.spk
     tj = ssp.t0 + t
     ts = toseconds(t)
+    b = ssp.bodies
     if length(ssp.bodies) >= 2
-        body1 = ssp.bodies[1]
-        body2 = ssp.origin == body1 ? ssp.bodies[2] : body1
+        body1 = b[1]
+        body2 = length(b) >= 2 ? b[2] : body1 != ssp.origin ? ssp.origin : ssp.inertial
         dir = position(spk, tj + tv, body1, body2)
         rot = cross(velocity(spk, tj + tv, body1, body2), position(spk, tj + tv, body1, body2))
     else
@@ -256,7 +263,9 @@ function startvalues(ssp::SolarSystemP, t::Period, tv::Period, h, vfactor)
     end
     v = sqrt(ssp.gravis[1] / norm(h)) * vfactor
     vel, pos = startvalues(h, v, dir, rot)
-    E = H([pos; ts], [vel; 0.0], ssp)
+    vel .+= velocity(spk, tj, ssp.inertial, body1)
+    pos .+= position(spk, tj, ssp.origin, body1)
+    E = 0.0 # H([pos; ts], [vel; 0.0], ssp)
     ([vel * ssp.m; E]), ([pos; ts])
 end
 
@@ -291,26 +300,41 @@ function on_approach(integrator)
     println("closest to moon: $(norm(R)) km t=$t s R=$R km")
 end
 
-function plotorbit(t0::TDBEpoch, tv::Period, n::Real, v, h=6731.0; dt = 300)
+function plotorbit(t0::TDBEpoch, tv::Period, n::Real, v, h=6731.0; dt = 300, id=2)
     ssp = SolarSystemP(spk, t0, earth_barycenter, earth, [earth, moon])
-    xaxis = [-350000.0, -300000.0]
-    yaxis = [-200000.0, -150000.0]
     prob = problem(ssp, (0days, n*days), tv, -h, v)
     sol = solve(prob, KahanLi8(), dt=dt, callback=cb_moonapproach())
     x = [sol(i*3600*12)[5] for i = 0:max(2n,1)]
     y = [sol(i*3600*12)[6] for i = 0:max(2n,1)]
-    p = plot!(sol, vars=(5,6), xaxis=xaxis, yaxis=yaxis, ratio=1.0)
-    scatter!(p, x, y, zcolor = (1:length(x)).*100)
+    xaxis, yaxis = axis(ssp, tv, id)
+    println(xaxis, " ", yaxis)
+    p = plot!(sol, vars=(5,6), ratio=1.0)
+    scatter!(p, x, y; xaxis, yaxis, zcolor = (1:length(x)).*100)
     display(p)
     sol
 end
 
+function axis(ssp, tv, i=2)
+    spk = ssp.spk
+    t0 = ssp.t0
+    tj = t0 + tv
+    if 1 <= i <= length(ssp.bodies)
+        r = JPLEphemeris.mean_radius(ssp.bodies[i])
+        pos = position(spk, tj, ssp.origin, ssp.bodies[i])
+    else
+        r = 4e5
+        pos = zeros(3)
+    end
+    [pos[1] - r, pos[1] + r] .+ [5000, 5000]
+    [pos[2] - r, pos[2] + r] .+ [5000, 5000]
+end
+
 function plotmoon(t0, n)
-    n = n * 12 
+    n = n * 12
     p = plot(legend=false, aspect_ratio=1.0)
     pos = [position(spk, t0 + (i/12)*days, earth, moon) for i = 0:n]
     x, y = getindex.(pos, 1), getindex.(pos, 2)
-    plot!(p, x, y)
+    plot!(p, x, y, xaxis=[-4e5,0], yaxis=[-4e5,1e5])
     scatter!(p, x[1:6:end], y[1:6:end], zcolor = 1:6:length(x))
     display(p)
 end
@@ -319,15 +343,38 @@ const t0 = TDBEpoch("2021-01-03T21:00:00")
 
 radius(sol, t) = begin st = sol(t); hypot(st[5], st[6], st[7]) end
 
-function ap(t, t0=t0)
-    sol=plotorbit(t0, t, 0.075, 0.995, dt=60)
-    findmax(radius.(Ref(sol), sol.t))[1]
+function eccentricity(t; t0=t0, v=1.0)
+    sol=plotorbit(t0, t, 0.075, v, dt=60)
+    a = /(extrema(radius.(Ref(sol), sol.t))...)
+    (1 - a) / (1 + a)
 end
 
-plotap(t0=t0) = plot([ap(i/2*days) for i = 0:56], legend=nothing)
+function ap(t; kwargs...)
+    ec = eccentricity(t; kwargs...)
+    2*ec / (1 + ec) * 6731
+end
+
+plotap(t0=t0; kwargs...) = plot([ap(i/2*days; kwargs...) for i = 0:56], legend=nothing)
+
+function relpos(sol, t, ori=earth)
+    spk = sol.prob.p.spk
+    orig = sol.prob.p.origin
+    tj = sol.prob.p.t0 + t*seconds
+    sol(t)[5:7] - position(spk, tj, orig, ori)
+end
+function relvel(sol, t, ori=earth)
+    spk = sol.prob.p.spk
+    orig = sol.prob.p.inertial
+    m = sol.prob.p.m
+    tj = sol.prob.p.t0 + t*seconds
+    sol(t)[1:3] / m - velocity(spk, tj, orig, ori)
+end
+
 
 const interesting_parameters = [
     (4.5days, -1.40090),
     (4.5days, +1.40250),
-    (4.0days, +1.40316),
+    (4.0days, +1.40220), # earth, moon
+    (4.0days, +1.40230), # with sun
+    (4.5days, +1.40175),
 ]
